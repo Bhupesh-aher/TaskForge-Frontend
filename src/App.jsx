564397742061be35d101ae29b5729be033df202b
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-10-06 19:32:19 +0530

    chore: setup vite react app with tailwind css and redux toolkit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center text-4xl font-bold text-blue-600">
+      TaskForge Frontend is Live 🚀
+    </div>
+  );
+}
+
+export default App
